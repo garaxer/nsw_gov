44c3c9ce78a368574b370febed2ccc25b6dfdee6
Gary Bagnall <gary.bag@live.com.au>
2025-09-30 09:58:12 +1000

    linting

--- handlers/src/clients/nsw-api.ts
+++ handlers/src/clients/nsw-api.ts
@@ -13,9 +13,9 @@ import {
 } from "./schema";
 import fetchWithTimeoutRetry from "./fetchWithTimeoutRetry";
 
-/** Builds the geocoding URL for a given address 
+/** Builds the geocoding URL for a given address
  * e.g. https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Geocoded_Addressing_Theme/FeatureServer/1/query?where=address+%3D+%27346%20PANORAMA%20AVENUE%20BATHURST%27&outFields=*&f=geojson
-*/
+ */
 const buildGeocodeUrl = (address: string) => {
   const safeAddress = address.toUpperCase().replace(/'/g, "''");
   const where = `address = '${safeAddress}'`;
@@ -25,7 +25,7 @@ const buildGeocodeUrl = (address: string) => {
 
 /** Builds the point query URL for given coordinates
  * e.g. https://portal.spatial.nsw.gov.au/server/rest/services/NSW_Administrative_Boundaries_Theme/FeatureServer/4/query?geometry=149.56705027261992%2C-33.42968429289573&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=false&f=geoJSON
-*/
+ */
 const buildPointQueryUrl = (
   baseUrl: string,
   longitude: number,
@@ -44,12 +44,12 @@ const buildPointQueryUrl = (
   return `${baseUrl}?${qs.toString()}`;
 };
 
-/** Fetches geocoded address data from NSW API 
+/** Fetches geocoded address data from NSW API
  * @param address - The address to geocode, e.g., "346 PANORAMA AVENUE BATHURST"
  * @returns GeocodeResponse containing features with coordinates and address properties
  * @throws NotFoundError if no results are found
  * @throws InternalServerError for API errors
-*/
+ */
 export const getGeocodedAddress = async (
   address: string
 ): Promise<GeocodeResponse> => {
